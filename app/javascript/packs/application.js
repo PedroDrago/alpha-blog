01c3f837d645256312c954349636ceece89237aa
drago-ubuntu <pedrodrago.profissional@gmail.com>
2022-10-23 09:56:21 -0300

    more conflicts?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import { environment } from "@rails/webpacker"
 
 Rails.start()
 Turbolinks.start()

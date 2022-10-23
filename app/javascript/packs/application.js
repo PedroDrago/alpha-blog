de8a370f442301fcfeb2062bd0d9f58154fa1367
drago-ubuntu <pedrodrago.profissional@gmail.com>
2022-10-23 07:04:39 -0300

    config: setup bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { environment } from "@rails/webpacker"
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()

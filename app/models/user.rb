ed36dd9a91577076aff099108620b4ff1eae174d
drago-ubuntu <pedrodrago.profissional@gmail.com>
2022-10-25 13:21:45 -0300

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-  #validates username:
-  #validates email:
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :username, 
+            uniqueness: { case_sensitive: false }, 
+            length: { minimum: 3, maximum: 25 }
+
+  validates :email, 
+            uniqueness: true, 
+            length: { maximum: 105 }, 
+            format: { with: VALID_EMAIL_REGEX }
 
 end

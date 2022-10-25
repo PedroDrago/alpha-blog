ab4e33a921e590356cb534840c5fc5728b3cdc6e
drago-ubuntu <pedrodrago.profissional@gmail.com>
2022-10-25 14:05:02 -0300

    create user article association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = 'Article was created successfully'
       redirect_to @article
@@ -23,7 +24,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
+  def edit 
 
   end
 

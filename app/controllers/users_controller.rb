856a1e7847f542bc3e97bd0ffa3ccfc2bfd0c743
drago-ubuntu <pedrodrago.profissional@gmail.com>
2022-10-27 16:58:13 -0300

    style: minor changes + change user pagination display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.paginate(page: params[:page], per_page: 14)
   end
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,8 @@ module ApplicationHelper
 
   def flash_class(level)
     case level.to_sym
-    when :notice then 'alert alert-success alert-dismissible fade show'
-    when :alert then 'alert alert-danger alert-dismissible fade show'
+    when :notice then 'alert-success'
+    when :alert then 'alert-danger'
     #when :error then 'alert alert-danger alert-dismissible fade show'
     #when :alert then 'alert alert-warning alert-dismissible fade show'
     

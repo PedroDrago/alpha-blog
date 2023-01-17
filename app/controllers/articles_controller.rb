1488440b10882b1382affb0263070a253db641a5
drago-ubuntu <pedrodrago.profissional@gmail.com>
2023-01-17 02:52:04 -0300

    refactor edit/new views and article controller methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
 
   end
 
   def show
-    @article = Article.find(params[:id])
 
   end
 
@@ -27,12 +28,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
 
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description))
       flash[:notice] = 'Article was updated successfully'
       redirect_to @article
@@ -42,10 +41,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = 'Article was deleted successfully'
     redirect_to articles_path
     
+  end
+
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+
   end
 end

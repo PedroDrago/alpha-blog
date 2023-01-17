bcb7eb3fdaf2cf5b7bb7bdc8cf2260c0f354d242
drago-ubuntu <pedrodrago.profissional@gmail.com>
2023-01-17 06:29:52 -0300

    create users table and validations

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  validates :username, 
+            presence: true, 
+            uniqueness: true , 
+            length: { minimum: 2, maximum: 15 }
+EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, 
+            presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 9, maximum: 150 },
+            format: { with: EMAIL_REGEX }
+
+end

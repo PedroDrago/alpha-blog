95038683e4895df1b8408fa1ab00b13fc8164987
drago-ubuntu <pedrodrago.profissional@gmail.com>
2023-01-17 13:00:14 -0300

    create user CRUD operations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level.to_sym
+    when :notice then 'alert-success'
+    when :alert then 'alert-danger'
+    #when :error then 'alert alert-danger alert-dismissible fade show'
+    #when :alert then 'alert alert-warning alert-dismissible fade show'
+    
+    
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  
   has_many :articles
   validates :username, 
             presence: true, 
@@ -11,4 +13,5 @@ EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
             length: { minimum: 9, maximum: 150 },
             format: { with: EMAIL_REGEX }
 
+  has_secure_password         
 end
